a710fc3ea3e72b3cb7281466b5dc7dec15cf0dee
Bryan Krausen <bkrausen@riverpointtechnology.com>
2025-03-20 14:58:39 -0400

    overhaul lab to simplify it

--- labs/lab_07_simplify_code_with_local_values/GITHUB/variables.tf
+++ labs/lab_07_simplify_code_with_local_values/GITHUB/variables.tf
@@ -1,9 +1,3 @@
-variable "organization" {
-  description = "GitHub organization name"
-  type        = string
-  default     = "your-organization" # Replace with your org name
-}
-
 variable "environment" {
   description = "Environment name for resource naming"
   type        = string
