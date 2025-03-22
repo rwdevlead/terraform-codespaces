f0142db3e86dd561ff4472ffe2c9dec0b6418b90
Bryan Krausen <bkrausen@riverpointtechnology.com>
2025-03-22 09:11:33 -0400

    updating lab_11

--- labs/lab_12_managing_resource_lifecycle_using_lifecyle/GITHUB/variables.tf
+++ labs/lab_12_managing_resource_lifecycle_using_lifecyle/GITHUB/variables.tf
@@ -1,9 +1,3 @@
-variable "github_owner" {
-  description = "GitHub owner (organization or username)"
-  type        = string
-  default     = "your-github-username" # Replace with your GitHub username or organization
-}
-
 variable "environment" {
   description = "Environment name"
   type        = string
@@ -13,5 +7,5 @@ variable "environment" {
 variable "repo_visibility" {
   description = "Repository visibility"
   type        = string
-  default     = "private"
+  default     = "public"
 }
