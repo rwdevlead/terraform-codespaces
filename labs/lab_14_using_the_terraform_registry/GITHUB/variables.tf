64144854d29b69996f5cf0869831eb6b27aa661e
Bryan Krausen <bkrausen@riverpointtechnology.com>
2025-03-22 13:03:58 -0400

    overhaul lab with different modules

--- labs/lab_14_using_the_terraform_registry/GITHUB/variables.tf
+++ labs/lab_14_using_the_terraform_registry/GITHUB/variables.tf
@@ -1,27 +1,29 @@
-variable "organization" {
-  description = "GitHub organization name"
+variable "repository_visibility" {
+  description = "Repo visibility configuration"
   type        = string
-  default     = "your-organization" # Replace with your org name or username
+  default     = "public"
 }
 
 variable "environment" {
   description = "Environment name"
   type        = string
-  default     = "dev"
+  default     = "development"
 }
 
-variable "repository_visibility" {
-  description = "Default repository visibility"
-  type        = string
-  default     = "private"
-}
-
-variable "teams" {
-  description = "Map of teams to create"
+variable "user_repos" {
+  description = "Map of users"
   type        = map(string)
   default = {
-    "developers" = "Repository developers team"
-    "operations" = "Infrastructure operations team"
-    "security"   = "Security reviewers team"
+    user1 = "kristen"
+    user2 = "aaron"
+    user3 = "jack"
+    user4 = "frank"
+    user5 = "monica"
   }
+}
+
+variable "repo_org" {
+  description = "Organization name"
+  type        = string
+  default     = "my-org"
 }
