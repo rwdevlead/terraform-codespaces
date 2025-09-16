9a0ff3b00e481529b414c2ae370d5ded6ea9f79c
Jim Stevens <18383118+rwdevlead@users.noreply.github.com>
2025-09-16 00:08:33 +0000

    Lab Day 1

---
+++ labs/terraform/providers.tf
@@ -0,0 +1,13 @@
+terraform {
+  required_version = ">= 1.13.2" # Replace with your installed version
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
