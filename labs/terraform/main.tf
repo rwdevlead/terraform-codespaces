5978e760a3337a20b6697bbaea1359c6c0eac366
Jim Stevens <18383118+rwdevlead@users.noreply.github.com>
2025-09-18 23:43:10 +0000

    lab day 2

---
+++ labs/terraform/main.tf
@@ -0,0 +1,12 @@
+# Create the primary VPC for workloads
+resource "aws_vpc" "main" {
+  cidr_block           = "192.168.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true
+
+  tags = {
+    Name        = "terraform-course"
+    Environment = "learning-terraform"
+    Managed_By  = "Terraform"
+  }
+}
